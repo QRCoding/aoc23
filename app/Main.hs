2bc4c95811e0a3305b6d5ee03d740d23dee206c9
Quinten Rutgers <quintenrutgers@upcmail.nl>
2024-01-15 15:35:54 +0100

    Solved Day23 part 1

--- app/Main.hs
+++ app/Main.hs
@@ -15,7 +15,7 @@ module Main where
 -- makeHeatMap :: [Int] -> HeatMap
 -- makeHeatMap ns = M.fromList $ concat [ [ ((y,x),ns!!(y*140+x)) | x <- [0..140]] | y <- [0..140]]
 
-import Day22 (answer1)
+import Day23 (answer1, answer2)
 
 main :: IO ()
-main = answer1 True >>= print
+main = answer2 True >>= print
